bbcc546603dd8c01e5686b824d99f074fb916d1b
massita99 <italia99>
2019-02-06 18:43:04 +0300

    HW5-2 fix: add ThreadLocal and Refactoring

--- L05reflection/src/test/java/ru/massita/framework/runner/TestRunner.java
+++ L05reflection/src/test/java/ru/massita/framework/runner/TestRunner.java
@@ -13,33 +13,32 @@ import java.util.List;
 
 public class TestRunner {
 
-    private static int testCount = 0;
+    private static ThreadLocal<Integer> testCount = new ThreadLocal<>();
 
-    private static List<String> errors = new LinkedList<>();
+    private static ThreadLocal<List<String>> errors = new ThreadLocal<>();
 
-    private static TestStatus status = TestStatus.PASSED;
+    private static ThreadLocal<TestStatus> status = new ThreadLocal<>();
 
     public static <T> void run(Class<T> clazz) {
         init();
         try {
-            T object = clazz.newInstance();
-
             List<Method> testMethods = ReflectionHelper.getMethodsByAnnotation(clazz, Test.class);
             List<Method> beforeEachMethods = ReflectionHelper.getMethodsByAnnotation(clazz, BeforeEach.class);
             List<Method> afterEachMethods = ReflectionHelper.getMethodsByAnnotation(clazz, AfterEach.class);
             for (Method method : testMethods) {
-                testCount++;
-                executeBefore(beforeEachMethods, object);
+                testCount.set(testCount.get() + 1);
+                T object = clazz.newInstance();
+                executeEach(beforeEachMethods, object);
                 executeTest(method, object);
-                executeAfter(afterEachMethods, object);
+                executeEach(afterEachMethods, object);
             }
         } catch (TestExecutionException | InstantiationException | IllegalAccessException e) {
-            status = TestStatus.ERROR;
+            status.set(TestStatus.ERROR);
             e.printStackTrace();
         }
     }
 
-    private static void executeBefore(List<Method> beforeEachMethods, Object object) throws TestExecutionException {
+    private static void executeEach(List<Method> beforeEachMethods, Object object) throws TestExecutionException {
         for (Method method : beforeEachMethods) {
             try {
                 method.invoke(object);
@@ -51,43 +50,33 @@ public class TestRunner {
         }
     }
 
-    private static void executeAfter(List<Method> beforeEachMethods, Object object) throws TestExecutionException {
-        for (Method method : beforeEachMethods) {
-            try {
-                method.invoke(object);
-            } catch (InvocationTargetException | IllegalAccessException e ) {
-                throw new TestExecutionException(String.format("Method %s in class %s failed. Execution Stopped.",
-                        method.getName(),
-                        object.getClass().getName()),e);
-            }
-        }
-    }
+
 
     private static void init() {
-        testCount = 0;
-        errors = new LinkedList<>();
-        status = TestStatus.PASSED;
+        testCount.set(0);
+        errors.set(new LinkedList<>());
+        status.set(TestStatus.PASSED);
     }
 
     private static void executeTest(Method method, Object object) {
         try {
             method.invoke(object);
         } catch (InvocationTargetException | IllegalAccessException e ) {
-            status = TestStatus.FAILED;
-            errors.add(e.toString());
+            status.set(TestStatus.FAILED);
+            errors.get().add(e.toString());
         }
     }
 
 
     public static int getTestCount() {
-        return testCount;
+        return testCount.get();
     }
 
     public static List<String> getErrors() {
-        return errors;
+        return List.copyOf(errors.get());
     }
 
     public static TestStatus getStatus() {
-        return status;
+        return status.get();
     }
 }
